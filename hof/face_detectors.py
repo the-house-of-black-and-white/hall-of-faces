e14b24cd06b9b4cd9d9cc3c1b6189caedc430608
fabito <fuechi@ciandt.com>
2018-03-12 23:48:24 -0300

    add VJ and YOLOv2 face detectors

--- hof/face_detectors.py
+++ hof/face_detectors.py
@@ -1,5 +1,5 @@
 import logging
-from abc import ABCMeta, abstractproperty
+from abc import ABCMeta, abstractmethod, abstractproperty
 
 import cv2
 import numpy as np
@@ -10,7 +10,44 @@ from hof.downloader import Downloader
 log = logging.getLogger(__name__)
 
 
-class BaseTensorflowFaceDetector:
+class FaceDetector:
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def detect(self, image, include_score=False, draw_faces=True, color=(0, 255, 0), min_confidence=None):
+        pass
+
+    @abstractproperty
+    def name(self):
+        pass
+
+    @abstractproperty
+    def model_file_name(self):
+        pass
+
+    @abstractproperty
+    def google_drive_doc_id(self):
+        pass
+
+    def download_and_extract_model(self, target_dir):
+        downloader = Downloader(target_dir)
+        downloader.download_file_from_google_drive(destination=self.model_file_name, id=self.google_drive_doc_id)
+        downloader.extract_tar_file(destination=target_dir, zip_file_name=self.model_file_name)
+
+    def draw_face(self, img, face, color):
+        # bbox face
+        x, y, w, h = face
+        label = self.name
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+        label_size, base_line = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        cv2.rectangle(img, (x, y), (x + label_size[0], y + label_size[1] + base_line), color, cv2.FILLED)
+        cv2.putText(img, label, (x, y + label_size[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
+
+class BaseTensorflowFaceDetector(FaceDetector):
     __metaclass__ = ABCMeta
 
     def __init__(self, min_confidence, checkpoint):
@@ -31,18 +68,6 @@ class BaseTensorflowFaceDetector:
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
             self.sess = tf.Session(graph=self.detection_graph)
-
-    @abstractproperty
-    def name(self):
-        pass
-
-    @abstractproperty
-    def model_file_name(self):
-        pass
-
-    @abstractproperty
-    def google_drive_doc_id(self):
-        pass
 
     def detect(self, image, include_score=True, draw_faces=True, color=(0, 255, 0), min_confidence=None):
         # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
@@ -86,20 +111,6 @@ class BaseTensorflowFaceDetector:
                     self.draw_face(image, (x, y, w, h), color)
 
         return faces
-
-    def draw_face(self, img, face, color):
-        # bbox face
-        x, y, w, h = face
-        label = self.name
-        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-        label_size, base_line = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-        cv2.rectangle(img, (x, y), (x + label_size[0], y + label_size[1] + base_line), color, cv2.FILLED)
-        cv2.putText(img, label, (x, y + label_size[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-
-    def download_and_extract_model(self, target_dir):
-        downloader = Downloader(target_dir)
-        downloader.download_file_from_google_drive(destination=self.model_file_name, id=self.google_drive_doc_id)
-        downloader.extract_tar_file(destination=target_dir, zip_file_name=self.model_file_name)
 
 
 class RfcnResnet101FaceDetector(BaseTensorflowFaceDetector):
@@ -155,3 +166,89 @@ class SSDMobileNetV1FaceDetector(BaseTensorflowFaceDetector):
 
     def __init__(self, min_confidence, checkpoint='models/ssd_mobilenet_v1_106650/frozen_inference_graph.pb'):
         super(SSDMobileNetV1FaceDetector, self).__init__(min_confidence, checkpoint=checkpoint)
+
+
+class YOLOv2FaceDetector(FaceDetector):
+    inWidth = 544
+    inHeight = 544
+    inScaleFactor = 1 / float(255)
+
+    @property
+    def name(self):
+        return 'YOLOv2'
+
+    @property
+    def model_file_name(self):
+        return 'yolo-widerface-1400.tar.gz'
+
+    @property
+    def google_drive_doc_id(self):
+        return '1_Uj59hkJEpht2ykZphW4m-l42odwkPJB'
+
+    def __init__(self, min_confidence):
+        super(YOLOv2FaceDetector, self).__init__()
+        self.min_confidence = min_confidence
+        self.cfg = 'models/yolo/yolo-obj.cfg'
+        self.model = 'models/yolo/yolo-face_1400.weights'
+        self.names = 'models/yolo/obj.names'
+        self.net = cv2.dnn.readNetFromDarknet(self.cfg, self.model)
+        if self.net.empty():
+            raise ValueError('Could not load net')
+
+    def detect(self, image, include_score=False, draw_faces=True, color=(0, 255, 0), min_confidence=None):
+        blob = cv2.dnn.blobFromImage(image, self.inScaleFactor, (self.inWidth, self.inHeight), (0, 0, 0), True, False)
+        self.net.setInput(blob)
+        detections = self.net.forward()
+        rows, cols, _ = image.shape
+        faces = []
+        for i in range(detections.shape[0]):
+            confidence = detections[i, 5]
+            if confidence > self.min_confidence:
+                x_center = detections[i, 0] * cols
+                y_center = detections[i, 1] * rows
+                width = detections[i, 2] * cols
+                height = detections[i, 3] * rows
+                xmin = int(round(x_center - width / 2))
+                ymin = int(round(y_center - height / 2))
+                if include_score:
+                    faces.append([xmin, ymin, int(width), int(height), confidence])
+                else:
+                    faces.append([xmin, ymin, int(width), int(height)])
+
+                if draw_faces:
+                    self.draw_face(image, (xmin, ymin, int(width), int(height)), color)
+        return faces
+
+
+class ViolaJonesFaceDetector(FaceDetector):
+
+    @property
+    def model_file_name(self):
+        pass
+
+    @property
+    def google_drive_doc_id(self):
+        pass
+
+    @property
+    def name(self):
+        return 'ViolaJones'
+
+    def __init__(self):
+        super(ViolaJonesFaceDetector, self).__init__()
+        self.scale_factor = 1.1
+        self.min_neighbors = 3
+        self.min_size = (30, 30)
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+    def detect(self, frame, include_score=False, draw_faces=True, color=(0, 255, 0), min_confidence=None):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.equalizeHist(gray, gray)
+        faces = self.face_cascade.detectMultiScale(gray, scaleFactor=self.scale_factor, minNeighbors=self.min_neighbors,
+                                                   minSize=self.min_size, flags=cv2.CASCADE_SCALE_IMAGE)
+
+        if draw_faces:
+            for face in faces:
+                self.draw_face(frame, face, color)
+
+        return faces
